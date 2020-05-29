d5e34c19748c729bff8737cfe484bf3817748cae
doge <shadowphoenixcoc@gmail.com>
2020-05-29 11:24:39 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ const { Client: { plugin } } = require('klasa');
 
 module.exports = {
 	KlasaGuild: require('./lib/extensions/KlasaGuild'),
-	KlasaGuildMemberStore: require('./lib/extensions/KlasaGuildMemberStore'),
+	KlasaGuildMemberManager: require('./lib/extensions/KlasaGuildMemberManager'),
 	KlasaMember: require('./lib/extensions/KlasaMember'),
 	MemberGateway: require('./lib/settings/MemberGateway'),
 	Client: require('./lib/Client'),
 	[plugin]: require('./lib/Client')[plugin]
 };
-
 /**
  * @external Guild
  * @see {@link https://discord.js.org/#/docs/main/master/class/Guild}
